c04d1d4cd30b0f35c3508a7e9a4e5627f8675369
Marcin Lewandowski <marcin@radiokit.org>
2018-10-30 09:42:16 +0100

    Changed port to 8000 as the app has no permissions to bind to the port < 1024

--- sentry.conf.py
+++ sentry.conf.py
@@ -247,7 +247,7 @@ if env('SENTRY_USE_SSL', False):
     SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = 80
+SENTRY_WEB_PORT = 8000 # It will not allow to bind to 80 due to reduced permissions
 SENTRY_WEB_OPTIONS = {
     # 'workers': 3,  # the number of web workers
 }
